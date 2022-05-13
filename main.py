24aaa973ea52f1161dff3c3fe2db3277a62de9fe
Baikal <37935787+BFssw@users.noreply.github.com>
2022-05-13 07:43:37 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 from flask import Flask
-app = Flask(__name__)
 from flask_gzip import Gzip
+
+
+app = Flask(__name__)
 gzip = Gzip(app)
+
 
 @app.route('/',methods=['GET','POST'])
 def head():
     return "OK"
-
-@app.route('/home',methods=['GET','POST'])
-def head():
-    return "home"
-
